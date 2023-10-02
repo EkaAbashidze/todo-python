5c7f0d9673e79ab5ffba3eba0ada14584a3c837a
Eka <eka.abashidze@gmail.com>
2023-10-02 20:05:49 +0400

    feat: create title length showing program

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-user_prompt = "Enter a todo: "
+title = input("Enter a title: ")
 
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
-
-todos = [todo1, todo2, todo3]
-print(todos)
-
-print(type(todos))
+print(len(title))
