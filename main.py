cd133f7592118353c0b399dc41fb445f6f3a3b02
Eka <eka.abashidze@gmail.com>
2023-10-03 17:07:39 +0400

    feat: add space removing functionality to the user_action

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, or exit: ")
+    user_action = input("Type add, show, or exit: ").strip()
     match user_action:
         case "add":
             todo = input("Enter a todo: ")
