5b48c0c1dd78d7214154ebbd90bd491291fad28a
Eka <eka.abashidze@gmail.com>
2023-10-03 18:19:23 +0400

    feat: add editing functionality to the todo item

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, or exit: ").strip().lower()
+    user_action = input("Type add, show, edit or exit: ").strip().lower()
     match user_action:
         case "add":
             todo = input("Enter a todo: ")
             todos.append(todo)
+        case "edit":
+            number = int(input("Number of the todo to edit: "))
+            editing = input(f'Enter a new text for this todo: {todos[number - 1]}')
+            todos[number - 1] = editing
+            print(todos[number - 1])
         case "show":
             for index, item in enumerate(todos):
                 item = item.title()
