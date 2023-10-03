126ff5767e7116e6ecc47710942326ccd67cfed1
Eka <eka.abashidze@gmail.com>
2023-10-03 16:57:34 +0400

    feat: add exit functionality separately

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 todos = []
 
 while True:
-    user_action = input("Type add or show")
+    user_action = input("Type add, show, or exit: ")
     match user_action:
         case "add":
             todo = input("Enter a todo: ")
             todos.append(todo)
         case "show":
             print(todos)
+            break
+        case "exit":
+            break
